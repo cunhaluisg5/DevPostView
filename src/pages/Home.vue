a5f0ef27549c0fdd894c2baa338ae7fa805c4493
Luís Cipriani <cunhaluisg5@gmail.com>
2024-03-12 20:21:17 -0300

    Cria página Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,61 @@
 <template>
   <div id="home">
-    <h1>Home</h1>
+      
+      <div class="newpost">
+        <h2>Bem vindo de volta!</h2>
+        <span>Compartilhe seu dia</span>
+        <textarea
+        placeholder="O que está fazendo hoje?"
+        rows="15"
+        >
+        </textarea>
+        <button>Compartilhar</button>
+      </div>
+
+      <div class="postarea">
+        <article class="post">
+          <h1>Matheus</h1>
+          <p>
+            Olá este é meu primeiro post aqui na plataforma sujeitoPost :)
+          </p>
+
+          <div class="action-post">
+            <button>20 curtidas</button>
+            <button>Veja post completo</button>
+          </div>
+        </article>
+
+        <article class="post">
+          <h1>Lucas Oliveira</h1>
+          <p>
+            Olá este é meu primeiro post aqui na plataforma sujeitoPost :)
+          </p>
+
+          <div class="action-post">
+            <button>20 curtidas</button>
+            <button>Veja post completo</button>
+          </div>
+        </article>
+      </div>
+
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Home_'
+  name: 'Home_',
+
 }
 </script>
 
 <style scoped>
+ #home{
+   display: flex;
+   flex-direction: row;
+   margin: 25px;
+ }
+
+ @import './home.css';
 
 </style>
