c86acc82f500d6b12e13fbbae0b671bd1f91dbab
Luís Cipriani <cunhaluisg5@gmail.com>
2024-03-18 19:49:53 -0300

    Cria dashboard

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,73 +1,39 @@
 <template>
   <div id="dashboard">
+      
       <h2>Minha Conta</h2>
       <span>Atualize seu perfil</span>
 
+      <form>
+        <label>Nome:</label>
+        <input type="text" v-model="nome" id="name" />
 
-      <form @submit.prevent="updatePerfil">
-        <label for="name">Nome:</label>
-        <input v-model.trim="nome" type="text" :placeholder="user.nome" id="name" />
-
-        <button class="button">Atualizar Perfil</button>
+        <button class="button" type="submit">Atualizar Perfil</button>
       </form>
 
-      <button class="button logout" @click="logOut">Deslogar</button>
+      <button class="button logout" @click="logOut">Sair</button>
+
   </div>
 </template>
 
 <script>
 import firebase from '../services/firebaseConnection';
+
 export default {
-  name:'Dashboard_',
-  data() {
-    return {
-      user: {},
-      nome:'',
-      title: '',
-      showSuccess: false
+  name: 'Dashboard_',
+  data(){
+    return{
+      nome: ''
     }
   },
-  created(){
-    const user = localStorage.getItem('sujeito');
-    this.user = JSON.parse(user);
-  },
   methods:{
-    async updatePerfil() {
-      if(this.nome === ''){
-        return;
-      }
-
-      await firebase.firestore().collection('users')
-      .doc(this.user.uid).update({
-        nome: this.nome
-      })
-
-      // Atualizando todos os posts do usuario
-      const postDocs = await firebase.firestore().collection('posts')
-      .where('userId', '==', this.user.uid).get()
-
-      
-      //percorrendo todos posts para achar todos e mudar o nome dentro
-      postDocs.forEach(async doc => {
-        await firebase.firestore().collection('posts').doc(doc.id).update({
-          autor: this.nome
-        })
-      })
-
-      //Atualizar nosso localStorege
-      localStorage.setItem('sujeito', JSON.stringify({uid: this.user.uid, nome: this.nome}));
-
-
-      alert('Perfil atualizado :)');
-    },
     async logOut(){
-
       const confirm = window.confirm('Você realmente deseja sair?');
 
       if(confirm){
         await firebase.auth().signOut()
-        .then(async ()=>{
-          await localStorage.removeItem('sujeito');
+        .then(async ()=> {
+          await localStorage.removeItem('devpost');
           this.$router.push('/login');
         })
       }else{
@@ -76,76 +42,70 @@ export default {
 
     }
   }
+
 }
 </script>
 
 <style scoped>
-#dashboard {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 60px;
-  padding: 15px 20px;
-  width: 500px;
-  max-width: 600px;
-  background: #4C5059;
-  box-shadow: 0 0 10px rgba(0,0,0, 0.2);
-}
+    #dashboard{
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin-top: 60px;
+      padding: 15px 20px;
+      widows: 600px;
+      max-width: 600px;
+      background: #4c5059;
+      box-shadow: 0 0 10px rgba(0,0,0, 0.2);
+    }
 
-h2{
-  color:#FFF;
-}
+    h2{
+      color: #FFF;
+    }
 
-span{
-  color:#FFF;
-  margin-bottom: 25px;
-}
+    span{
+      color: #FFF;
+      margin-bottom: 25px;
+    }
 
-form{
-  width: 500px;
-  display: flex;
-  flex-direction: column;
-}
+    form{
+      width: 500px;
+      display: flex;
+      flex-direction: column;
+    }
 
-form label{
-  margin-right: 5px;
-  font-size: 18px;
-  color: #FFF;
-  padding-bottom: 5px
-}
-form input{
-  height: 30px;
-  padding: 5px;
-  font-size: 17px;
-  margin-right: 5px;
-  margin-bottom: 0px;
-  background: #FAFAFA;
-  border:0;
-  outline: none;
-}
+    form label{
+      font-size: 18px;
+      color: #FFF;
+      padding-bottom: 5px;
+    }
 
-button{
-  cursor: pointer;
-  margin-top:15px;
-  height: 35px;
-  border:0;
-  background: #7289DA;
-  color: #FFF;
-  font-size: 16px;
-}
+    form input{
+      height: 30px;
+      padding: 5px;
+      font-size: 17px;
+      border:0;
+      outline: none;
+      margin-bottom: 5px;
+      background: #FAFAFA;
+   }
 
-button.logout{
-  width: 500px;
-  background: #E36D6D;
-  color: #FFF;
-  font-size: 16px;
-}
+   button{
+     cursor: pointer;
+     margin-top: 10px;
+     height: 35px;
+     border:0;
+     background: #7289DA;
+     color: #FFF;
+     font-size: 17px;
+   }
 
-::placeholder{
-  opacity:1;
-  color: #424242;
-}
+   button.logout{
+     width: 500px;
+     background: #E36D6D;
+     color: #FFF;
+   }
 
 
 </style>
